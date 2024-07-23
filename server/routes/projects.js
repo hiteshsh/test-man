60c7700c44f35b891a53504bc7f1a902bd79006e
Hitesh <45hitesh@gmail.com>
2024-07-23 15:57:39 +0530

    fixing issue with wrong resource mentioned for auth check

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -11,9 +11,26 @@ import { authorize } from "../middleware/verifyJWT.js";
 
 const router = express.Router();
 
-router.get("/projects",authorize([{ resource: "project", action: "view" }]), getProjects);
-router.post("/project", authorize([{ resource: "release", action: "addEdit" }]),validate("createProject"), createProject);
-router.delete("/project/:projectId", authorize([{ resource: "release", action: "delete" }]),deleteProjectById);
-router.put("/project/:projectId",authorize([{ resource: "release", action: "addEdit" }]), updateProjectById);
+router.get(
+  "/projects",
+  authorize([{ resource: "project", action: "view" }]),
+  getProjects
+);
+router.post(
+  "/project",
+  authorize([{ resource: "project", action: "addEdit" }]),
+  validate("createProject"),
+  createProject
+);
+router.delete(
+  "/project/:projectId",
+  authorize([{ resource: "project", action: "delete" }]),
+  deleteProjectById
+);
+router.put(
+  "/project/:projectId",
+  authorize([{ resource: "project", action: "addEdit" }]),
+  updateProjectById
+);
 
 export default router;
